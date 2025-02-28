dd9d3e572611d386b8843dcb5524508a8027851f
advanced-bencoding <benjamin.joseph212620@gmail.com>
2025-03-01 00:06:15 +0530

    feat: add JournalEntry component and enhance Home layout with Tamagui button

---
+++ app/(home)/entries/[id].tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native"
+import { Text } from "tamagui"
+
+const JournalEntry = () => {
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}
+
+export default JournalEntry;
--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,10 +1,17 @@
 import { View } from "react-native";
-import { Text } from "tamagui";
+import { Button, Text } from "tamagui";
+import { Plus } from "@tamagui/lucide-icons";
 
 const Home = () => {
     return (
-        <View>
+        <View style={{ backgroundColor: "yellow", flex: 1, paddingHorizontal: 20, paddingVertical: 10 }}>
             <Text>Home</Text>
+            <Button
+                icon={<Plus color="white" size={30} />}
+                circular position="absolute"
+                style={{ top: "93%", left: "93%", backgroundColor: "green" }}
+                size="$5"
+            />
         </View>
     )
 }
